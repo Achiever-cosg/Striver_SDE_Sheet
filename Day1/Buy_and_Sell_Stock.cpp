4e9411c5400d9ece25b386a39f90351789c64f1f
Achiever-cosg <93369157+Achiever-cosg@users.noreply.github.com>
2022-03-17 09:07:19 +0530

    Create Buy_and_Sell_Stock.cpp

---
+++ Day1/Buy_and_Sell_Stock.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.size() <= 1)
+            return 0;
+        int minPrice = INT_MAX;
+        int maxProfit = 0;
+        
+        for(int i=0; i<prices.size(); i++)
+        {
+            minPrice = min(minPrice, prices[i]);
+            maxProfit = max(maxProfit, prices[i] - minPrice);
+        }
+        return maxProfit;
+    }
+};
